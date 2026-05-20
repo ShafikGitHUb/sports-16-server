82c3e3b8c536d3d170a4a257e31d5b7aa493289a
ShafikGitHUb <princeshafik11@gmail.com>
2026-05-20 11:03:40 +0600

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv")
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config()
 const uri = process.env.MONGODB_URI
-
 const app = express();
 const port = process.env.PORT
+app.use(cors())
+app.use(express.json())
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -17,14 +19,31 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
+//create database
+const db = client.db("sports")
+const facilityCollection = db.collection("facilities")
+
+//get method allfacility data add
+app.get("/all-facilities", async(req,res)=>{
+  const result = await facilityCollection.find().toArray()
+  res.send(result)
+})
+
+//addfacility server post method
+app.post("/add-facility", async (req, res) => {
+  const newFacility = req.body;
+  const result = await facilityCollection.insertOne(newFacility);
+  res.send(result);
+});
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
 app.get("/", (req, res) => {
   res.send("Sports16 Server Running");
 });
